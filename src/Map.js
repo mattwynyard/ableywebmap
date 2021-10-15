59dccfd18c1471eca9aea2b4d980aa3fdbc28d8d
mjwynyard@gmail.com <mjwynyard@gmail.com>
2021-10-16 08:00:14 +1300

    added feature layer from feature server - fixed scalebar units issue

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import './App.css';
 import WebMap from "@arcgis/core/WebMap";
 // import Map from "@arcgis/core/Map";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import MapView from "@arcgis/core/views/MapView";
 import Home from "@arcgis/core/widgets/Home";
 import ScaleBar from "@arcgis/core/widgets/ScaleBar";
@@ -32,13 +33,15 @@ function Map() {
 
             let scalebar = new ScaleBar({
                 view: mapView,
-                units: "metric"
+                unit: "metric",
+                style: "ruler"
             });
             let compass = new Compass({
                 view: mapView
               });
             let layerlist = new LayerList({
                 view: mapView
+
             });
             let basemapToggle = new BasemapToggle({
                 view: mapView,
@@ -47,15 +50,20 @@ function Map() {
             let homeWidget = new Home({
             view: mapView
             });
+            let layer = new FeatureLayer({
+                url: "https://services7.arcgis.com/jI87xPT7G1AGV8Uo/ArcGIS/rest/services/LINZ_NZ_Property_Titles/FeatureServer/0"
+              });
+
             mapView.ui.add(homeWidget, "top-left");
             mapView.ui.add(scalebar, "bottom-right");
             mapView.ui.add(compass, "top-left");
             mapView.ui.add(layerlist, "top-right");
             mapView.ui.add(basemapToggle, "bottom-left");
+            map.add(layer)
+
             mapView.when(() => {
                 console.log("loaded")
-                scalebar.units = "metric"
-            })
+            });
         }
   }, []);
 
